325a51eaace9fab34183905b1c5a2b68ded2430c
Kenichi <kenichixie@gmail.com>
2019-01-22 01:30:52 +0700

    fix config

--- config/config.js
+++ config/config.js
@@ -17,7 +17,7 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: "127.0.0.1",
+    host: process.env.HOST,
     dialect: "mysql"
   }
 };
